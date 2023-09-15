f92f09abc7c8a729cf0bd8f0eba01a14bd5f27e3
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-09-15 22:46:35 +0300

    Implement logout Add CSRF tokens to avoid forged requests attack

---
+++ src/main/java/com/drodionov/learning/springoauth2/web/api/CsrfController.java
@@ -0,0 +1,15 @@
+package com.drodionov.learning.springoauth2.web.api;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfController {
+
+  @GetMapping("/csrf")
+  public CsrfToken csrf(CsrfToken csrfToken) {
+    return csrfToken;
+  }
+
+}
--- src/main/java/com/drodionov/learning/springoauth2/web/config/WebConfig.java
+++ src/main/java/com/drodionov/learning/springoauth2/web/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.drodionov.learning.springoauth2.web.config;
 
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.csrf.CookieCsrfTokenRepository.withHttpOnlyFalse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +11,20 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-public class WebSecurityConfigAdapter {
+public class WebConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
         .authorizeHttpRequests(
-            (auth) -> auth.requestMatchers("/", "/index.html", "/error", "/webjars/**").permitAll().anyRequest()
-                .authenticated()
-        ).exceptionHandling(
+            (auth) -> auth.requestMatchers("/", "/csrf", "/index.html", "/error", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+        .exceptionHandling(
             exception -> exception.authenticationEntryPoint(new HttpStatusEntryPoint(UNAUTHORIZED)))
+        .csrf(csrfConfigurer -> csrfConfigurer.csrfTokenRepository(withHttpOnlyFalse()))
+        .logout(logout -> logout.logoutSuccessUrl("/").permitAll())
         .oauth2Login(withDefaults())
         .build();
   }
